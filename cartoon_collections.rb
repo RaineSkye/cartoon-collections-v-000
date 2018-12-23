6a1056ce40e0ea11230bc0d50d0468d0ff27f4a9
kitte anderson <kitteanderson@gmail.com>
2018-12-23 00:29:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def roll_call_dwarves(array)# code an argument here
   end
 end
 
-def summon_captain_planet(planeteer_calls)# code an argument here
-  planeteer_calls.collect{|e| e.capitalize + "!"}
+def summon_captain_planet# code an argument here
+  array.collect{|e| e.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
