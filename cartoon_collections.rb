1da40dda98638fc4247f8e89a8f35806a9d12c58
kitte anderson <kitteanderson@gmail.com>
2018-12-23 01:49:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def long_planeteer_calls(array)
   end
 end
 
-long_planeteer_calls(array){|i| i.length > 4}
+long_planeteer_calls(array).any?{|i| i.length >= 4}
 
 def find_the_cheese (array)
   # the array below is here to help
