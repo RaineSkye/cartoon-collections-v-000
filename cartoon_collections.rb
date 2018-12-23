b004265d4cd0a1fbb94983f10be94bc54f01d8dc
kitte anderson <kitteanderson@gmail.com>
2018-12-23 02:05:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,20 @@
 def roll_call_dwarves(array)
-  i = 1
-  array.each_with_index do |names|
-    puts "#{i}. #{names}"
-    i += 1
+  array.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  #i = 1
+  #array.each_with_index do |names|
+    #puts "#{i}. #{names}"
+    #i += 1
   end
 end
 
 def summon_captain_planet(array)
   array.collect{|e| e.capitalize + "!"}
+  #array.collect{|e| e.capitalize << "!"}
 end
 
-def long_planeteer_calls(array)
-  array.any?{|word| word.length > 4}
+def long_planeteer_calls(calls)
+  calls.any?{|i| i.length > 4}
 end
 
 def find_the_cheese (array)
