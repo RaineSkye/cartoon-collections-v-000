05585407a10c58e8ebb0c649e46adf0e9a9a0d00
kitte anderson <kitteanderson@gmail.com>
2018-12-23 00:37:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each{|call| call > 4}
+  array.each{|call| call > 4.length}
 end
 
 def find_the_cheese# code an argument here
