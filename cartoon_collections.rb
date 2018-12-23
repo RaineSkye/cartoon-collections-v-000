e1cfb2e2a011e49c959b6c98c3677122a1847e4b
kitte anderson <kitteanderson@gmail.com>
2018-12-23 01:58:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 def find_the_cheese (array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?("cheddar", "gouda", "camembert")
+  array.detect?("cheddar", "gouda", "camembert")
 
 
 end
