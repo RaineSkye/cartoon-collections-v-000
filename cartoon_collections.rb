7b52af9d4eb0993bca73a100e5f074db6b478eab
kitte anderson <kitteanderson@gmail.com>
2018-12-23 01:30:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,13 +11,16 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each do |call| if call.size < 4 return false
-  else 
-  true 
-end
+  i = 0
+  while i < array.length
+    if yield(array[i])
+      return array[i]
+    end
+    i += 1
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese (array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
