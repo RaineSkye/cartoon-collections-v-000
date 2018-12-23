78055eb2cb803b0cec9959f20cccfbe4bfc8c5b4
kitte anderson <kitteanderson@gmail.com>
2018-12-23 01:51:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,16 +11,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  i = 0
-  while i < array.length
-    if yield(array[i])
-      return array[i]
-    end
-    i += 1
-  end
+  array.any?{|word| word.length > 4}
 end
-
-long_planeteer_calls(array).any?{|i| i.length >= 4}
 
 def find_the_cheese (array)
   # the array below is here to help
